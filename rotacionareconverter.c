3beb609b77d873354853d7b3bb37513af63d4a9c
naiieandrade <andradenaiara@hotmail.com>
2018-04-28 11:30:09 -0300

    pass int vector binary to decimal

--- rotacionareconverter.c
+++ rotacionareconverter.c
@@ -3,10 +3,11 @@
 #include<math.h>
 #include<string.h>
 
+#define MAXSIZEBIN 9
+
 int main(){
-  int vetor [3][3] = {0,0,1,0,1,0,0,1,0};
-  char array [9];// = {0};
-  char array_char[9][2];
+  int vetor [3][3] = {0,1,0,1,1,0,0,1,1};
+  int array [MAXSIZEBIN] = {0};
 
   for(int i=0; i<3; i++){
     for(int j=0; j<3; j++){
@@ -31,32 +32,22 @@ int main(){
     array[i+7]=vetor[1][i];
   }
 
-  for(int i=0; i<9;i++)
+  for(int i=0; i<MAXSIZEBIN;i++)
     printf("%d ", array[i]);
 
-  /*for(int i=0;i<9;i++){
-    int num;
-    char let[1];
-    num = array[i];
-    sprintf(let, "%d", num);
-    strncpy(array_char[i], let, 2);
-  }*/
-
-  //int s;
-  int iiii = 0;
+  int i = 0;
   unsigned int dec = 0;
 
-  int s = 9;//strlen( array );
-  //s = 9;
-//  printf("%c\n", );
+  int s = MAXSIZEBIN;
+
   printf("\nS = %d\n", s );
   while( s-- ) {
-      if( array[s] == '0' || array[s] == '1' ) {
-          dec = dec + pow(2, iiii++) * (array[s] - '0');
-          printf("dec = %u, s=%d\n", dec,s);
+      if( array[s] == 0 || array[s] == 1 ) {
+          dec = dec + pow(2, i++) * (array[s] - 0);
+          //printf("dec = %u, s=%d\n", dec,s);
       }
   };
-  printf("\nDecimal Equivalent of Binary Number: \t %u\n", dec);
+  printf("\nDecimal: \t %u\n", dec);
 
   return 0;
 }
