0506e9f506a579c45881e661c973df3aab91dce6
zero101010 <igotaraujo97@gmail.com>
2018-05-10 09:28:10 -0300

    open the file

--- projeto3.c
+++ projeto3.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
-#include <conio.h>
+
 
 typedef struct contatos
   {
     char nome[101];
     char celular[30];
     unsigned int CEP;
-    char data_nascimento[];  
+    char data_nascimento[10];  
     struct contatos *prox;
     struct contatos *ant;
 }agenda;
 
 
 
-int main(void)
-{    
+int main(void){
+  char arquivo [] = "contatos.txt";
+  FILE *file;
+  file = fopen(arquivo, "r");
 
+  if(file==NULL){
+	    printf("Falha!\n");
+	} else {
+      printf("arquivo lido com sucesso\n");
+  }
   
-  };
+}
