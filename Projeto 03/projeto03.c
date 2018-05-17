75e2d9af99afb08d289164d2fe09e4537e29bc90
Victor <guin-hu@hotmail.com>
2018-05-16 21:53:57 -0300

    Adicionando lista da struct

--- Projeto 03/projeto03.c
+++ Projeto 03/projeto03.c
@@ -2,18 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+#define novo 1
+#define remover 2
+#define buscar 3
+#define visualizar 4
+#define sair 0 
 
-    char nome_completo[101];
-    char celular[14];
-    unsigned int cep;
-    char endereco[40];
-    char data_nascimento[11];  
-    struct Agenda *proximo;
-    struct Agenda *anterior;
-}Agenda;
+struct registro {
+  char nome_completo[101];
+  char telefone[11];
+  char endereco[101];
+  unsigned int cep;
+  char data_nascimento[11];
+  struct registro *prox;
+};
+typedef struct registro Agenda;
 
-int main () {
+int vazia(Agenda *agenda) {
+
+	if(agenda->prox == NULL) { 
+	
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void imprime(Agenda *agenda){
+
+	if(vazia(agenda)) {
+		
+		printf("Lista vazia\n");
+	} else {
+
+		Agenda *tmp;
+		tmp = agenda->prox;
+	
+	while( tmp != NULL){
+		printf("%s", tmp->nome_completo);
+		tmp = tmp->prox;
+	}
+	
+	printf("\n\n");
+	}
+}
+
+Agenda *insere_struct(Agenda *agenda,int tamanho) {
+
+	Agenda *proximo = agenda->prox;
+	
+	Agenda *novo_contato = (Agenda*)malloc(sizeof(Agenda));
+
+	for(int aux = 0; aux < tamanho; aux ++) {
+
+		strcpy(novo_contato->nome_completo,agenda[aux].nome_completo);
+		strcpy(novo_contato->telefone,agenda[aux].telefone);
+		strcpy(novo_contato->endereco,agenda[aux].endereco);
+		novo_contato->cep,agenda[aux].cep;
+		strcpy(novo_contato->data_nascimento,agenda[aux].data_nascimento);
+		novo_contato->prox = agenda;
+	}
+
+	return novo_contato;
+}
+
+void ler_arquivo() {
 
 	char arquivo [] = "contatos.txt";
 	int qtdeLinhas = 0;
@@ -36,6 +89,7 @@ int main () {
 	        
 	        	qtdeLinhas++;
 	        } if(caracteres=='$') {
+
 	        	qtdeDolar++;
 	        }
 	    }		
@@ -49,21 +103,29 @@ int main () {
 
 		fscanf(file, "%[^\n]\n %[^\n]\n %[^\n]\n %d\n %[^\n]\n %[^\n]\n", 
 			agenda[aux].nome_completo, 
-			agenda[aux].celular, 
+			agenda[aux].telefone, 
 			agenda[aux].endereco,
 			&agenda[aux].cep,
 			agenda[aux].data_nascimento,
 			lixo);
 	}
 
-	//for(int aux = 0; aux < total; aux ++) {
-
-		printf("Nome: %s\n", agenda[2].nome_completo);
-		printf("Celular: %s\n", agenda[2].celular);
-		printf("Endereco: %s\n", agenda[2].endereco);
-		printf("CEP: %d\n", agenda[2].cep);
-		printf("DATA DE NASCIMENTO: %s\n", agenda[2].data_nascimento);
-	//}
+	insere_struct(agenda,qtdeDolar);
 	
+	fclose(file);
+}
+
+void inicializa_lista(Agenda *agenda) {
+
+	agenda->prox = NULL;
+}
+
+int main () {
+
+	Agenda * agenda = (Agenda *) malloc(sizeof(agenda));
+
+	inicializa_lista(agenda);
+	ler_arquivo();
+	free(agenda);
 	return 0;
 }
