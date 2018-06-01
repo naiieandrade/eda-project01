0151dea485240ce42c795cc35c2bb3f32662a593
MrVictor42 <guin-hu@hotmail.com>
2018-06-01 15:14:29 -0300

    Arquivo txt na fila

--- project04.c
+++ project04.c
@@ -121,139 +121,6 @@ void imprime (Fila* fila) {
 	}
 }
 
-int main () {
-
- 	Fila* fila = cria();
- 	push(fila,"Victor",42);
- 	push(fila,"Isabela",42);
- 	push(fila,"Naruto",42);
- 	push(fila,"teste",42);
-
- 	printf("Fila:\n");
- 	imprime(fila);
- 	libera(fila);
- 	
- 	return 0;
-}
-
-/*struct aviao {
-
-    char codigo[CODIGO_AVIAO];
-    int combustivel;
-    struct aviao *prox;
-};
-typedef struct aviao Aviao;
-
-struct fila {
-
-	struct Aviao *inicio;
-	struct Aviao *final;
-};
-typedef struct fila Fila;
-
-Fila* cria () {
-	
-	Fila* novo = (Fila*) malloc(sizeof(Fila));
- 	
- 	novo->inicio = novo->final = NULL;
- 	
- 	return novo;
-}
-
-Aviao* insere_inicio(Aviao *aviao,char codigo_aviao[CODIGO_AVIAO],int combustivelA) {
-
-	Aviao *novo = (Aviao*) malloc(sizeof(Aviao));
-
-	strcpy(novo->codigo,codigo_aviao);
-	novo->combustivel = combustivelA;
-
-	novo->prox = NULL;
-
-	if(aviao != NULL) {
-		aviao->prox = novo;
-	}
-
-	return novo;
-}
-
-Aviao* retira_inicio(Aviao *aviao) {
-
-	Aviao *retira = aviao->prox;
-	free(aviao);
-
-	return retira;
-}
-
-void insere (Fila* f, char codigo_aviao[CODIGO_AVIAO],int combustivelA) {
- f->final = insere_inicio(f->final,codigo_aviao,combustivelA);
- if (f->inicio==NULL) /* fila antes vazia? 
- f->inicio = f->final;
-}
-
-/*Aviao *inicializa_lista(){
-  
-    return NULL;
-}
-
-Pista_de_decolagem *cria() {
-
-	Pista_de_decolagem *pista_de_decolagem = (Pista_de_decolagem*) malloc(sizeof(Pista_de_decolagem));
-
-	pista_de_decolagem->inicio = pista_de_decolagem->final = NULL;
-
-	return pista_de_decolagem;
-}
-
-Aviao *insere_inicio(Aviao *aviao,char codigo_aviao[CODIGO_AVIAO],int combustivelA) {
-
-	Aviao *novo = (Aviao*) malloc(sizeof(Aviao));
-
-	strcpy(novo->codigo,codigo_aviao);
-	novo->combustivel = combustivelA;
-
-	novo->prox = NULL;
-
-	if(aviao != NULL) {
-		aviao->prox = novo;
-	}
-
-	return novo;
-}
-
-Aviao *retira_inicio(Aviao *aviao) {
-
-	Aviao *retira = aviao->prox;
-	free(aviao);
-
-	return retira;
-}
-
-void push(Pista_de_decolagem *pista_de_decolagem,char codigo_aviao[CODIGO_AVIAO],int combustivelA) {
-
-	pista_de_decolagem->inicio = insere_inicio(pista_de_decolagem->final,codigo_aviao,combustivelA);
-}
-
-void imprime(Aviao *aviao){
-
-    Aviao *voos;
-
-    if(aviao==NULL) {
-        
-        printf("\n\nNão possui nenhum registro :(\n");
-    } else {
-
-        printf("\n\nVisualizar registros:\n");    
-    }
-    
-    for(voos=aviao; voos!=NULL; voos=voos->prox){
-        
-        printf("Código: %s\n", voos->codigo);
-        printf("Combustivel: %d\n", voos->combustivel);
-    }
-
-    printf("\n\n\n");
-}
-
 int gerar_numero(int lim_inf, int lim_sup){
 	
 
@@ -268,7 +135,9 @@ int gerar_numero(int lim_inf, int lim_sup){
 
 int main () {
 
-	int nVoos = 0,nAproximacoes = 0,nDecolagens = 0,combustivelA = 0;
+ 	Fila* fila = cria();
+
+ 	int nVoos = 0,nAproximacoes = 0,nDecolagens = 0,combustivelA = 0;
 	int uniTempo = 5;
 
     srand(time(0));
@@ -299,31 +168,18 @@ int main () {
 	qtdeLinhas +=1;
 
 	rewind(file);
-
-	Aviao *voos = (Aviao *)malloc(sizeof(Aviao));
-
-	if(!voos){
-
-        printf("Sem memória disponível!\n");
-        exit(1);
-    } else {
-
-    	voos = inicializa_lista();
     	
-		for(int aux = 0; aux < qtdeLinhas; aux ++) {
+	for(int aux = 0; aux < qtdeLinhas; aux ++) {
 
-			fscanf(file, "%[^\n]\n", codigo);
+		fscanf(file, "%[^\n]\n", codigo);
 			
-			combustivelA = gerar_numero(0,12);
-			//push(voos,codigo,combustivelA);
-		}    		
-    }
-
-    imprime(voos);
+		combustivelA = gerar_numero(0,12);
+		push(fila,codigo,combustivelA);
+	}    		
+    
 	fclose(file);
 
-
-	/*nAproximacoes = gerar_numero(10,32);
+	nAproximacoes = gerar_numero(10,32);
 	nDecolagens = gerar_numero(10,32);
 	combustivelA = gerar_numero(0,12);
 	nVoos = nAproximacoes + nDecolagens;
@@ -335,9 +191,11 @@ int main () {
 	printf("Numero combaA: %d\n", combustivelA);
 
     printf("HORA: %s\n",__TIME__);
+
+
+ 	printf("Fila:\n");
+ 	imprime(fila);
+ 	libera(fila);
+ 	
+ 	return 0;
 }
-
-int main () {
-
-	printf("HORA: %s\n",__TIME__);
-}*/
